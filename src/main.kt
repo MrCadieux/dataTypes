a723a1bd5c78e0c0b327e0e92a0500c855d1647b
Jeff Cadieux <jcadieux@d155.org>
2020-02-23 09:22:52 -0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val myInt: Int = 4
+    val myByte: Byte = 8
+    val myShort: Short = 56
+    var myLong: Long = 38475957974
+    var myFloat: Float = 45.567f
+    var myDouble: Double = 23.4
+    var myChar: Char = 'j'
+    var myBoolean: Boolean = true
+
+    println("Int = $myInt")
+    println("Double = $myDouble")
+    println("Long = $myLong")
+    println("Float = $myFloat")
+    println("Short = $myShort")
+    println("Byte = $myByte")
+    println("Char = $myChar")
+    println("Boolean = $myBoolean")
+}
